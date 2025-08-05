d4967a8402d390f65a483ce33bc4671dde19e142
junwoo <mkshie@naver.com>
2025-08-05 21:03:43 +0900

    create project

---
+++ src/main/java/com/test/buymebackend/BuyMeBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.test.buymebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyMeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyMeBackEndApplication.class, args);
+    }
+
+}
+
