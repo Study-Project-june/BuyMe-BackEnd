d6a6538cfd6180463f6d65d311b701006ddc3859
junwoo <mkshie@naver.com>
2025-08-11 18:36:26 +0900

    TODO 작성

--- src/main/java/com/test/buymebackend/domain/member/controller/AuthController.java
+++ src/main/java/com/test/buymebackend/domain/member/controller/AuthController.java
@@ -21,9 +21,10 @@ import org.springframework.web.multipart.MultipartFile;
 @Tag(name = "Auth", description = "회원 인증 API")
 public class AuthController {
     private final AuthService authService;
-
+    //TODO 회원가입 기능에 추가되지 않은 기능들 (이미지 파일 첨부 , request 중복 처리 , 비밀번호 encode 하기)
     @PostMapping(value = "/signup")
     @Operation(summary = "회원가입 API")
+    //TODO basereponse 형태 만들기 이후 통일하기
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "회원 가입에 성공했습니다.",
                     content = @Content(mediaType = "application/json")),
@@ -37,6 +38,8 @@ public class AuthController {
         authService.signup(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    //TODO spring security 와 jwt 로 로그인 기능 구현하기
     @PostMapping(value = "/login")
     @Operation(summary = "로그인 API")
     public ResponseEntity<Void> login(
