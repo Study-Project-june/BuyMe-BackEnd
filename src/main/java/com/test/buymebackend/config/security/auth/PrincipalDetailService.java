88f625604f3c30c09cd1994c97c25effee331e3a
junwoo <mkshie@naver.com>
2025-08-11 23:51:35 +0900

    feat : member 엔티티 로그인시 검증을 위한 설정

---
+++ src/main/java/com/test/buymebackend/config/security/auth/PrincipalDetailService.java
@@ -0,0 +1,23 @@
+package com.test.buymebackend.config.security.auth;
+
+import com.test.buymebackend.domain.member.entity.Member;
+import com.test.buymebackend.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        return new PrincipalDetail(member);
+    }
+}
