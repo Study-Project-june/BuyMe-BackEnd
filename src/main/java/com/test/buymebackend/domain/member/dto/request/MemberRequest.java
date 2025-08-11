1c7e47b900d4ba61974bedb33473bc4aef922cc8
junwoo <mkshie@naver.com>
2025-08-11 23:51:48 +0900

    feat : member 엔티티 로그인시 검증을 위한 설정

--- src/main/java/com/test/buymebackend/domain/member/dto/request/MemberRequest.java
+++ src/main/java/com/test/buymebackend/domain/member/dto/request/MemberRequest.java
@@ -26,4 +26,16 @@ public class MemberRequest {
         private String name;
 
     }
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LoginRequest {
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
+        private String email;
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        private String password;
+    }
 }
