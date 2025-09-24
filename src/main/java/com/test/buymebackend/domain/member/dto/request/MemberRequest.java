0363e705678d356d20a6ba7236ed5b0761e6dd70
junwoo <mkshie@naver.com>
2025-09-25 03:00:30 +0900

    chore : swagger 에서 예시값 나오게 수정

--- src/main/java/com/test/buymebackend/domain/member/dto/request/MemberRequest.java
+++ src/main/java/com/test/buymebackend/domain/member/dto/request/MemberRequest.java
@@ -1,6 +1,7 @@
 package com.test.buymebackend.domain.member.dto.request;
 
 import com.test.buymebackend.domain.member.validation.annotation.UniqueEmail;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -19,6 +20,7 @@ public class MemberRequest {
     public static class SignUpRequest {
         @NotBlank(message = "이메일은 필수입니다.")
         @Email(message = "올바른 이메일 형식이 아닙니다.")
+        @Schema(type = "string", example = "string@example.com")
         @UniqueEmail
         private String email;
 
@@ -37,6 +39,7 @@ public class MemberRequest {
     public static class LoginRequest {
         @NotBlank(message = "이메일은 필수입니다.")
         @Email(message = "올바른 이메일 형식이 아닙니다.")
+        @Schema(type = "string", example = "string@example.com")
         private String email;
 
         @NotBlank(message = "비밀번호는 필수입니다.")
